b9382fc10891d5c17f5816072402d9a577f0fc86
Pythonblyat <dansmolovik31@gmail.com>
2026-09-17 12:22:10 +0300

    dada

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+@FunctionalInterface
+interface Transformer<T, R> {
+
+    R transform(T input);
+
+    default void printResult(T input) {
+        R result = transform(input);
+        System.out.println("Результат перетворення: " + result);
+    }
+
+    static boolean isNotNull(Object obj) {
+        return obj != null;
+    }
+}
+
+class Main {
+    // В нових версіях Java можна просто void main()
+    void main() {
+
+        // Використовуємо method reference (String::length) замість лямбди
+        Transformer<String, Integer> stringLength = String::length;
+        stringLength.printResult("Привіт, Java!");
+
+        Transformer<Integer, Integer> doubleNumber = num -> num * 2;
+        doubleNumber.printResult(15);
+
+        System.out.println("Перевірка 1 (не null?): " + Transformer.isNotNull("Текст є"));
+        System.out.println("Перевірка 2 (не null?): " + Transformer.isNotNull(null));
+    }
+}
